557bfc26eb069a95830ec76a36d3b76bffa660bf
oscar-ricaud <Oscar.Ricaud@ibm.com>
2020-07-20 10:46:59 -0500

    updated home page image, it was blurry on large devices

--- src/gatsby-theme-carbon/templates/Homepage.js
+++ src/gatsby-theme-carbon/templates/Homepage.js
@@ -3,7 +3,7 @@ import { HomepageBanner, HomepageCallout } from 'gatsby-theme-carbon';
 import HomepageTemplate from 'gatsby-theme-carbon/src/templates/Homepage';
 import { calloutLink } from './Homepage.module.scss';
 
-import Carbon from '../../images/homepage.jpg';
+import Carbon from '../../images/homepage-v2.jpg';
 
 const FirstLeftText = () => <p>Storefront Application</p>;
 
